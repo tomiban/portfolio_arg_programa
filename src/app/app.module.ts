e46f01f6f587f471a1b3961a5bfccc7e365c872f
tomiban <81537786+tomiban@users.noreply.github.com>
2022-03-17 16:44:06 -0300

    Se agrega el componente header con logo, banner y social media

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
+import { SocialmediaComponent } from './components/socialmedia/socialmedia.component';
+import { BannerComponent } from './components/banner/banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoAPComponent,
+    SocialmediaComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
