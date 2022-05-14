40941db9787557d586f36bfeebc6dc1dc8ea2b7e
tomiban <81537786+tomiban@users.noreply.github.com>
2022-05-14 18:59:37 -0300

    Se arma la estructura backend y se conecta con el servidor

---
+++ BackEnd/src/main/java/comportfolioARGPROG/tb/TbApplication.java
@@ -0,0 +1,13 @@
+package comportfolioARGPROG.tb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TbApplication.class, args);
+	}
+
+}
