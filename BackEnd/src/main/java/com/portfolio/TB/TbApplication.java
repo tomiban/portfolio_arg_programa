061b985350bdace11c90fc97195c4f5b87e3f5cd
tomiban <81537786+tomiban@users.noreply.github.com>
2022-05-14 12:27:19 -0300

    Se crea la carpeta BackEnd

---
+++ BackEnd/src/main/java/com/portfolio/TB/TbApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.TB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TbApplication.class, args);
+	}
+
+}
