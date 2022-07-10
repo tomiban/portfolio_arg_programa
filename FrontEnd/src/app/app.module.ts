f8400e356fc36b9ba30c8d64793bd091e8c15152
tomiban <81537786+tomiban@users.noreply.github.com>
2022-07-10 20:47:32 -0300

    Revert "routing y login front end"
    This reverts commit a5f4698fb0a19522052114163a809691f1accedb.

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -17,9 +17,6 @@ import { ProyectsComponent } from './components/proyects/proyects.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
-import { HomeComponent } from './components/home/home.component';
-import { LoginComponent } from './components/login/login.component';
-
 
 @NgModule({
   declarations: [
@@ -35,8 +32,6 @@ import { LoginComponent } from './components/login/login.component';
     SkillsComponent,
     ProyectsComponent,
     FooterComponent,
-    HomeComponent,
-    LoginComponent
   ],
   imports: [
     BrowserModule,
--- FrontEnd/src/app/components/logo-ap/logo-ap.component.ts
+++ FrontEnd/src/app/components/logo-ap/logo-ap.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-logo-ap',
@@ -8,11 +7,9 @@ import { Router } from '@angular/router';
 })
 export class LogoAPComponent implements OnInit {
 
-  constructor(private router:Router) { }
+  constructor() { }
 
   ngOnInit(): void {
   }
-  login() {
-    this.router.navigate(['/login']);
-  }
+
 }
